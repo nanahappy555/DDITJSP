05c59e5939ef2cee5902f1adb0f20cac4dbb25de
nanahappy555 <nanahappy555@gmail.com>
2022-07-14 21:30:57 +0900

    7/14 servlet

---
+++ servlet_basic/WebContent/WEB-INF/views/colorForm.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>배경색바꾸기</title>
+</head>
+<body>
+	<h1>colorForm.jsp</h1>
+	<form method="post" >
+		<input type="text" name="color" />
+		<input type="submit" value="변경" />
+	</form>
+</body>
+</html>
--- servlet_basic/src/com/jsp/controller/Gugudan.java
+++ servlet_basic/src/com/jsp/controller/Gugudan.java
@@ -13,15 +13,21 @@ import javax.servlet.http.HttpServletResponse;
 public class Gugudan extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String danStr = request.getParameter("dan");
+		String gopStr = request.getParameter("gop");
+		
+		int danNum = Integer.parseInt(danStr);
+		int gopNum = Integer.parseInt(gopStr);
+		
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter(); //콘솔->화면
 		
-		for(int dan=2; dan<10; dan++) {
+		for(int dan=2; dan<danNum; dan++) {
 			//System.out.println(dan+"단");
-			out.println(dan+"단<br/>");
-			for(int gop=1; gop<10; gop++) {
+			out.println(danNum+"단<br/>");
+			for(int gop=1; gop<gopNum; gop++) {
 				//System.out.println(dan+"*"+gop+"="+(dan*gop));
-				out.println(dan+"*"+gop+"="+(dan*gop)+"<br/>");
+				out.println(danNum+"*"+gopNum+"="+(danNum*gopNum)+"<br/>");
 			}
 //			System.out.println();
 			out.println("<br/>");
